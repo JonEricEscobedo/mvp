a7dc02f3925164ba02b637dd8801de7b0abdab80
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-04-28 11:57:29 -0700

    Successful API call to DarkSky

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var API = require('./src/config/config.js');
+var request = require('request');
 
 app.use(express.static('./'))
 
@@ -10,6 +11,13 @@ app.get('/', function(req, res) {
 
 app.post('/weather', function(req, res) {
   console.log('Inside /weather');
+  request({
+    method: 'GET',
+    uri: `https://api.darksky.net/forecast/${API.KEY}/37.8267,-122.4233`
+  }, function(error, response, body) {
+    console.log(body);
+  })
+
   res.end();
 });
 
