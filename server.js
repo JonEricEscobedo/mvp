20efb83b19f4a1fcd895dc20a28735000aadcd56
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-04-28 11:49:20 -0700

    POST request from client to server is working

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var API = require('./src/config/config.js');
 
 app.use(express.static('./'))
 
 app.get('/', function(req, res) {
   res.send('Hello World, up and running!');
+});
+
+app.post('/weather', function(req, res) {
+  console.log('Inside /weather');
+  res.end();
 });
 
 app.listen(1337, function() {
--- src/directives/App.js
+++ src/directives/App.js
@@ -2,6 +2,22 @@ angular.module('weather-go', [])
 
 .directive('app', function() {
   return {
+    controllerAs: 'ctrl',
+    bindToController: true,
+    controller: ($http) => {
+      this.fetchWeather = () => {
+        $http({
+          method: 'POST',
+          url: '/weather'
+        })
+        .then(function successCallback(response) {
+          console.log('Successful POST request');
+        }, function errorCallback(error) {
+          console.log('Error in POST request');
+        });
+      }
+      this.fetchWeather();
+    },
     templateUrl: '/src/templates/App.html'
   }
 })
